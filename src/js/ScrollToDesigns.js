2ec9705d98e4f45c7c882acfe6f4611fecd577a0
Medicino <105019072+xTheHeatx@users.noreply.github.com>
2022-11-07 09:41:41 +0400

    !

--- src/js/ScrollToDesigns.js
+++ src/js/ScrollToDesigns.js
@@ -19,32 +19,32 @@ function ScrollToDesigns() {
 
         designs.classList.add('rendered')
 
-        if (width <= 3840 && height <= 2160) {
-            window.scrollTo({ top: 2430, behavior: 'smooth' })
-        }
+        // if (width >= 3840 && height >= 2160) {
+        //     window.scrollTo({ top: 2430, behavior: 'smooth' })
+        // }
 
-        if (width <= 1920 && height <= 1080) {
-            window.scrollTo({ top: 1220, behavior: 'smooth' })
-        }
+        // if (width <= 1920 && height <= 1080) {
+        //     window.scrollTo({ top: 1220, behavior: 'smooth' })
+        // }
 
-        if (width <= 1360 && height <= 768) {
-            window.scrollTo({ top: 860, behavior: 'smooth' })
-        }
+        // if (width <= 1360 && height <= 768) {
+        //     window.scrollTo({ top: 860, behavior: 'smooth' })
+        // }
 
-        if (width <= 1280 && height <= 720) {
-            window.scrollTo({ top: 820, behavior: 'smooth' })
-        }
+        // if (width <= 1280 && height <= 720) {
+        //     window.scrollTo({ top: 820, behavior: 'smooth' })
+        // }
 
-        if (width <= 415) {
-            window.scrollTo({ top: 952, behavior: 'smooth' })
-        }
+        // if (width == 414) {
+        //     window.scrollTo({ top: 980, behavior: 'smooth' })
+        // }
 
-        if (width <= 395) {
+        if (width == 375 && height == 667) {
             window.scrollTo({ top: 950, behavior: 'smooth' })
         }
 
-        if (width <= 380) {
-            window.scrollTo({ top: 950, behavior: 'smooth' })
+        if (width == 320 && height == 568) {
+            window.scrollTo({ top: 2000, behavior: 'smooth' })
         }
 
 
