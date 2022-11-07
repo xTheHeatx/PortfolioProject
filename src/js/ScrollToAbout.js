05d2dd0740bdcae29b0f931739d0edbd9abd950e
Medicino <105019072+xTheHeatx@users.noreply.github.com>
2022-11-07 10:45:19 +0400

    !

--- src/js/ScrollToAbout.js
+++ src/js/ScrollToAbout.js
@@ -24,33 +24,45 @@ function ScrollToAbout() {
 
         footer.classList.add('rendered')
 
-        if (width <= 3840 && height <= 2160) {
-            window.scrollTo({ top: 4365, behavior: 'smooth' })
+        // Desctop
+
+        if (width === 3840 && height === 2160) {
+            window.scrollTo({ top: 4845, behavior: 'smooth' })
         }
 
-        if (width <= 1920 && height <= 1080) {
-            window.scrollTo({ top: 2200, behavior: 'smooth' })
+        if (width === 2560 && height === 1440) {
+            window.scrollTo({ top: 3230, behavior: 'smooth' })
         }
 
-        if (width <= 1360 && height <= 768) {
-            window.scrollTo({ top: 1545, behavior: 'smooth' })
+        if (width === 1920 && height === 1080) {
+            window.scrollTo({ top: 2430, behavior: 'smooth' })
+        }
+
+        if (width === 1600 && height === 900) {
+            window.scrollTo({ top: 2020, behavior: 'smooth' })
+        }
+
+        if (width === 1536 && height === 864) {
+            window.scrollTo({ top: 1950, behavior: 'smooth' })
+        }
+
+        if (width === 1440 && height === 900) {
+            window.scrollTo({ top: 975, behavior: 'smooth' })
+        }
+
+        if (width === 1366 && height === 768) {
+            window.scrollTo({ top: 1725, behavior: 'smooth' })
+        }
+
+        if (width === 1360 && height === 768) {
+            window.scrollTo({ top: 1720, behavior: 'smooth' })
         }
 
         if (width <= 1280 && height <= 720) {
-            window.scrollTo({ top: 1455, behavior: 'smooth' })
+            window.scrollTo({ top: 1620, behavior: 'smooth' })
         }
 
-        if (width <= 380) {
-            window.scrollTo({ top: 1950, behavior: 'smooth' })
-        }
-
-        if (width <= 395) {
-            window.scrollTo({ top: 1950, behavior: 'smooth' })
-        }
-
-        if (width <= 415) {
-            window.scrollTo({ top: 1952, behavior: 'smooth' })
-        }
+        // Mobile
 
 
 
--- src/js/ScrollToDesigns.js
+++ src/js/ScrollToDesigns.js
@@ -19,36 +19,46 @@ function ScrollToDesigns() {
 
         designs.classList.add('rendered')
 
-        // if (width >= 3840 && height >= 2160) {
-        //     window.scrollTo({ top: 2430, behavior: 'smooth' })
-        // }
 
-        // if (width <= 1920 && height <= 1080) {
-        //     window.scrollTo({ top: 1220, behavior: 'smooth' })
-        // }
+        // Desctop
 
-        // if (width <= 1360 && height <= 768) {
-        //     window.scrollTo({ top: 860, behavior: 'smooth' })
-        // }
-
-        // if (width <= 1280 && height <= 720) {
-        //     window.scrollTo({ top: 820, behavior: 'smooth' })
-        // }
-
-        // if (width == 414) {
-        //     window.scrollTo({ top: 980, behavior: 'smooth' })
-        // }
-
-        if (width == 375 && height == 667) {
-            window.scrollTo({ top: 950, behavior: 'smooth' })
+        if (width <= 3840 && height <= 2160) {
+            window.scrollTo({ top: 2430, behavior: 'smooth' })
         }
 
-        if (width == 320 && height == 568) {
-            window.scrollTo({ top: 2000, behavior: 'smooth' })
+        if (width <= 2560 && height <= 1440) {
+            window.scrollTo({ top: 1620, behavior: 'smooth' })
         }
 
+        if (width <= 1920 && height <= 1080) {
+            window.scrollTo({ top: 1220, behavior: 'smooth' })
+        }
 
+        if (width <= 1600 && height <= 900) {
+            window.scrollTo({ top: 1020, behavior: 'smooth' })
+        }
 
+        if (width <= 1536 && height <= 864) {
+            window.scrollTo({ top: 975, behavior: 'smooth' })
+        }
+
+        if (width <= 1440 && height <= 900) {
+            window.scrollTo({ top: 975, behavior: 'smooth' })
+        }
+
+        if (width <= 1366 && height <= 768) {
+            window.scrollTo({ top: 870, behavior: 'smooth' })
+        }
+
+        if (width <= 1360 && height <= 768) {
+            window.scrollTo({ top: 860, behavior: 'smooth' })
+        }
+
+        if (width <= 1280 && height <= 720) {
+            window.scrollTo({ top: 810, behavior: 'smooth' })
+        }
+
+        // Mobile
 
 
 
